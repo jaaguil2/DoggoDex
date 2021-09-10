5b4623f2c5bdfd845382897c2a872ab95eb0302f
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-09-10 11:34:48 -0400

    added mock data

--- doggodex/src/App.js
+++ doggodex/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  
+  // test data
+  const data = {
+    name: "dogdogdog",
+    image: "https://i.imgur.com/Psvkw19m.jpg?1",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      app
+    </main>
   );
-}
+};
 
 export default App;
